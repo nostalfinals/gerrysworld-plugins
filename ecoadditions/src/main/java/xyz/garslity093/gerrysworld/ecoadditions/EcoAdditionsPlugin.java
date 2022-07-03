a4c6f46362a7ef964c453a8b4e48bb6ae4cb8913
GerryYuu <jjjooo233@163.com>
2022-07-03 12:04:01 +0800

    codes

--- ecoadditions/src/main/java/xyz/garslity093/gerrysworld/ecoadditions/EcoAdditionsPlugin.java
+++ ecoadditions/src/main/java/xyz/garslity093/gerrysworld/ecoadditions/EcoAdditionsPlugin.java
@@ -7,6 +7,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class EcoAdditionsPlugin extends JavaPlugin{
 
     private static ItemStack coinItemStack;
@@ -17,7 +20,7 @@ public final class EcoAdditionsPlugin extends JavaPlugin{
     }
 
     /*初始化货币物品堆*/
-    public void initCoinItemStack() {
+    private void initCoinItemStack() {
         coinItemStack = new ItemStack(Material.SUNFLOWER, 1);
         ItemMeta coinItemMeta = coinItemStack.getItemMeta();
         coinItemMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&e&l货币"));
@@ -27,8 +30,14 @@ public final class EcoAdditionsPlugin extends JavaPlugin{
     }
 
     /*货币物品堆 Getter*/
-    public static ItemStack getCoinItemStack() {
-        return coinItemStack;
+    public static ItemStack getCoinItemStack(double amount) {
+        ItemMeta coinItemMeta = coinItemStack.getItemMeta();
+        List<String> loreList = new ArrayList<>();
+        loreList.add(ChatColor.translateAlternateColorCodes('&', "&fx " + amount));
+        coinItemMeta.setLore(loreList);
+        ItemStack patchedCoinItemMeta = coinItemStack.clone();
+        patchedCoinItemMeta.setItemMeta(coinItemMeta);
+        return patchedCoinItemMeta;
     }
 
     /*插件启动方法*/
