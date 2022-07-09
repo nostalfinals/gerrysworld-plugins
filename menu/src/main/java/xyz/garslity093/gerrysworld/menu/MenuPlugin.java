015a717f6df2f3a45178488b3ef6c6d9eae3f2b8
GerryYuu <jjjooo233@163.com>
2022-07-10 00:53:50 +0800

    添加 menu

---
+++ menu/src/main/java/xyz/garslity093/gerrysworld/menu/MenuPlugin.java
@@ -0,0 +1,14 @@
+package xyz.garslity093.gerrysworld.menu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MenuPlugin extends JavaPlugin {
+
+    public static JavaPlugin INSTANCE = JavaPlugin.getPlugin(MenuPlugin.class);
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
