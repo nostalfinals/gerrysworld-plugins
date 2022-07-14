2ef229f1058898e5ba156f2ab781ec6359dd267a
GerryYuu <jjjooo233@163.com>
2022-07-15 01:12:26 +0800

    新增 wgadditions

---
+++ wgadditions/src/main/java/xyz/garslity093/gerrysworld/wgadditions/WgAdditionsPlugin.java
@@ -0,0 +1,21 @@
+package xyz.garslity093.gerrysworld.wgadditions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @packageName: xyz.garslity093.gerrysworld.wgadditions
+ * @className: WgAdditionsPlugin
+ * @author: GerryYuu
+ * @date: 7/15/2022 1:11 AM
+ */
+
+public final class WgAdditionsPlugin extends JavaPlugin {
+    public static JavaPlugin getInstance() {
+        return JavaPlugin.getPlugin(WgAdditionsPlugin.class);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
